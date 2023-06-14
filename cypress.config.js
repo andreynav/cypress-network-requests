32710f2aaa13899d12e612f009e44d18f6d14f52
andreinalivaika <nav.testsw@gmail.com>
2023-06-14 10:56:09 +0300

    01 add network get request and change config

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,11 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+    defaultCommandTimeout: 5000,
+    baseUrl: "https://jsonplaceholder.cypress.io",
+    chromeWebSecurity: false,
+    viewportWidth: 1000,
+    viewportHeight: 800,
   },
 });
