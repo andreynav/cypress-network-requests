ad3d8e37c3c32444943ee45bbd1da35fa83a4e33
andreinalivaika <nav.testsw@gmail.com>
2023-06-15 12:33:09 +0300

    02 add network post request and change config

--- cypress/e2e/network-request.js
+++ cypress/e2e/network-request.js
@@ -13,7 +13,7 @@ describe("Network Requests", () => {
         cy.wait('@getComment').its("response.statusCode").should("eq", 200)
     })
 
-    it.only("Get Request with mocked response body", () => {
+    it("Get Request with mocked response body", () => {
         cy.intercept({
             method: "GET",
             url: "**/comments/*",
@@ -29,5 +29,27 @@ describe("Network Requests", () => {
         cy.wait('@getComment').its("response.statusCode").should("eq", 200);
         cy.get('@getComment').its("response.body.body").should("eq", 'You are Winner!!!')
         cy.get('@getComment').its("response.body.name").should("eq", 'Andy')
+    })
+
+    it("Post Request with intercept", () => {
+        cy.intercept("POST", "/comments").as("postComment")
+
+        cy.get('.network-post') .click()
+        cy.wait("@postComment").should(({request, response}) => {
+            console.log(request)
+            // console.log(response)
+
+            expect(request.body).to.include('email')
+            expect(request.body).to.include('name')
+            expect(request.headers).to.have.property('content-type')
+            // issue https://github.com/cypress-io/cypress/issues/14924
+            // expect(request.headers).to.have.property('origin', 'https://example.cypress.io')
+            expect(request.headers).to.have.property('content-length', '160')
+
+            expect(response.body).to.have.property( 'body','You can change the method used for cy.intercept() to be GET, POST, PUT, PATCH, or DELETE')
+            expect(response.body).to.have.property( 'name','Using POST in cy.intercept()')
+            expect(response.body).to.have.property( 'email','hello@cypress.io')
+        })
+
     })
 })
